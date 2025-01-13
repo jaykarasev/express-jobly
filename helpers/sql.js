8e6b852e47756df42712901d0c754a48da23ee2e
Jay Karasev <jaykarasev@gmail.com>
2025-01-13 17:48:19 -0600

    Part One Complete

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,14 +1,33 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/**
+ * Helper for making selective update queries.
+ *
+ * This helper is used to dynamically create part of an SQL query for updating
+ * specific fields in a database table.
+ *
+ * @param dataToUpdate {Object} {field1: newVal, field2: newVal, ...}
+ * @param jsToSql {Object} maps js-style data fields to database column names,
+ *   like { firstName: "first_name", age: "age" }
+ *
+ * @returns {Object} An object containing:
+ *   - `setCols` {string} - The SQL `SET` clause for the `UPDATE` statement, with
+ *     placeholders for parameterized queries. Example: `"first_name"=$1, "age"=$2`
+ *   - `values` {Array} - The array of values to be used with the placeholders
+ *     in the `SET` clause. Example: `["Aliya", 32]`
+ *
+ * @example {firstName: 'Aliya', age: 32} =>
+ *   { setCols: '"first_name"=$1, "age"=$2',
+ *     values: ['Aliya', 32] }
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
 
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
-  const cols = keys.map((colName, idx) =>
-      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+  const cols = keys.map(
+    (colName, idx) => `"${jsToSql[colName] || colName}"=$${idx + 1}`
   );
 
   return {
